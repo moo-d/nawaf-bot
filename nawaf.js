3a7b9a020f42823af381f213dc063c4804b98aac
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-10-20 07:55:42 +0700

    Update nawaf.js

--- nawaf.js
+++ nawaf.js
@@ -16,3 +16,31 @@ const cron = require('node-cron')
 
 const { msgFilter, color, processTime, isUrl, createSerial } = require('./tools')
 const { daily, level, afk, reminder, premium, limit} = require('./function')
+const cd = 4.32e+7
+const limitCount = 25
+const errorImg = 'https://i.ibb.co/jRCpLfn/user.png'
+const dateNow = moment.tz('Asia/Jakarta').format('DD-MM-YYYY')
+const ocrconf = {
+    lang: 'eng',
+    oem: '1',
+    psm: '3'
+}
+
+const _nsfw = JSON.parse(fs.readFileSync('./database/group/nsfw.json'))
+const _antilink = JSON.parse(fs.readFileSync('./database/group/antilink.json'))
+const _antinsfw = JSON.parse(fs.readFileSync('./database/group/antinsfw.json'))
+const _leveling = JSON.parse(fs.readFileSync('./database/group/leveling.json'))
+const _welcome = JSON.parse(fs.readFileSync('./database/group/welcome.json'))
+const _autosticker = JSON.parse(fs.readFileSync('./database/group/autosticker.json'))
+const _ban = JSON.parse(fs.readFileSync('./database/bot/banned.json'))
+const _premium = JSON.parse(fs.readFileSync('./database/bot/premium.json'))
+const _mute = JSON.parse(fs.readFileSync('./database/bot/mute.json'))
+const _registered = JSON.parse(fs.readFileSync('./database/bot/registered.json'))
+const _level = JSON.parse(fs.readFileSync('./database/user/level.json'))
+let _limit = JSON.parse(fs.readFileSync('./database/user/limit.json'))
+const _afk = JSON.parse(fs.readFileSync('./database/user/afk.json'))
+const _reminder = JSON.parse(fs.readFileSync('./database/user/reminder.json'))
+const _daily = JSON.parse(fs.readFileSync('./database/user/daily.json'))
+const _stick = JSON.parse(fs.readFileSync('./database/bot/sticker.json'))
+const _setting = JSON.parse(fs.readFileSync('./database/bot/setting.json'))
+let { memberLimit, groupLimit } = _setting
