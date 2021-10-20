e4cf425d1ef2e32b63c13df5e120524a88efe9c9
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-10-20 08:16:34 +0700

    Update nawaf.js

--- nawaf.js
+++ nawaf.js
@@ -1,21 +1,46 @@
+/********** MODULES **********/
 const { decryptMedia, Client } = require('@open-wa/wa-automate')
 const fs = require('fs-extra')
+const Nekos = require('nekos.life')
+const neko = new Nekos()
+const os = require('os')
+const nhentai = require('nhentai-js')
+const { API } = require('nhentai-api')
+const api = new API()
+const sagiri = require('sagiri')
+const NanaAPI = require('nana-api')
+const nana = new NanaAPI()
 const fetch = require('node-fetch')
+const isPorn = require('is-porn')
 const exec = require('await-exec')
-const config = require('./config.json')
+const config = require('../config.json')
+const saus = sagiri(config.nao, { results: 5 })
 const axios = require('axios')
+const tts = require('node-gtts')
+const nekobocc = require('nekobocc')
+const ffmpeg = require('fluent-ffmpeg')
+const bent = require('bent')
 const path = require('path')
 const ms = require('parse-ms')
 const toMs = require('ms')
 const canvas = require('canvacord')
 const mathjs = require('mathjs')
+const emojiUnicode = require('emoji-unicode')
 const moment = require('moment-timezone')
 const ocrtess = require('node-tesseract-ocr')
-moment.tz.setDefault('Saudi_Arabia/Qassim').locale('sau')
+const translate = require('@vitalets/google-translate-api')
+moment.tz.setDefault('Asia/Jakarta').locale('id')
+const genshin = require('genshin')
+const google = require('google-it')
 const cron = require('node-cron')
+/********** END OF MODULES **********/
 
-const { msgFilter, color, processTime, isUrl, createSerial } = require('./tools')
-const { daily, level, afk, reminder, premium, limit} = require('./function')
+/********** UTILS **********/
+const { msgFilter, color, processTime, isUrl, createSerial } = require('../tools')
+const { nsfw, weeaboo, downloader, fun, misc, toxic } = require('../lib')
+const { uploadImages } = require('../tools/fetcher')
+const { ind, eng } = require('./text/lang/')
+const { daily, level, register, afk, reminder, premium, limit} = require('../function')
 const cd = 4.32e+7
 const limitCount = 25
 const errorImg = 'https://i.ibb.co/jRCpLfn/user.png'
@@ -25,7 +50,9 @@ const ocrconf = {
     oem: '1',
     psm: '3'
 }
+/********** END OF UTILS **********/
 
+/********** DATABASES **********/
 const _nsfw = JSON.parse(fs.readFileSync('./database/group/nsfw.json'))
 const _antilink = JSON.parse(fs.readFileSync('./database/group/antilink.json'))
 const _antinsfw = JSON.parse(fs.readFileSync('./database/group/antinsfw.json'))
@@ -35,6 +62,7 @@ const _autosticker = JSON.parse(fs.readFileSync('./database/group/autosticker.js
 const _ban = JSON.parse(fs.readFileSync('./database/bot/banned.json'))
 const _premium = JSON.parse(fs.readFileSync('./database/bot/premium.json'))
 const _mute = JSON.parse(fs.readFileSync('./database/bot/mute.json'))
+const _registered = JSON.parse(fs.readFileSync('./database/bot/registered.json'))
 const _level = JSON.parse(fs.readFileSync('./database/user/level.json'))
 let _limit = JSON.parse(fs.readFileSync('./database/user/limit.json'))
 const _afk = JSON.parse(fs.readFileSync('./database/user/afk.json'))
@@ -43,3 +71,270 @@ const _daily = JSON.parse(fs.readFileSync('./database/user/daily.json'))
 const _stick = JSON.parse(fs.readFileSync('./database/bot/sticker.json'))
 const _setting = JSON.parse(fs.readFileSync('./database/bot/setting.json'))
 let { memberLimit, groupLimit } = _setting
+/********** END OF DATABASES **********/
+
+/********** MESSAGE HANDLER **********/
+// eslint-disable-next-line no-undef
+module.exports = msgHandler = async (bocchi = new Client(), message) => {
+    try {
+        const { type, id, from, t, sender, isGroupMsg, chat, caption, isMedia, mimetype, quotedMsg, quotedMsgObj, mentionedJidList } = message
+        let { body } = message
+        const { name, formattedTitle } = chat
+        let { pushname, verifiedName, formattedName } = sender
+        pushname = pushname || verifiedName || formattedName
+        const botNumber = await bocchi.getHostNumber() + '@c.us'
+        const blockNumber = await bocchi.getBlockedIds()
+        const ownerNumber = config.ownerBot
+        const authorWm = config.authorStick
+        const packWm = config.packStick
+        const groupId = isGroupMsg ? chat.groupMetadata.id : ''
+        const groupAdmins = isGroupMsg ? await bocchi.getGroupAdmins(groupId) : ''
+        const time = moment(t * 1000).format('DD/MM/YY HH:mm:ss')
+
+        const cmd = caption || body || ''
+        const command = cmd.toLowerCase().split(' ')[0] || ''
+        const prefix = /^[°•π÷×¶∆£¢€¥®™✓=|~!#$%^&./\\©^]/.test(command) ? command.match(/^[°•π÷×¶∆£¢€¥®™✓=|~!#$%^&./\\©^]/gi) : '-' // Multi-Prefix by: VideFrelan
+        const chats = (type === 'chat') ? body : ((type === 'image' || type === 'video')) ? caption : ''
+        body = (type === 'chat' && body.startsWith(prefix)) ? body : (((type === 'image' || type === 'video') && caption) && caption.startsWith(prefix)) ? caption : ''
+        const args = body.trim().split(/ +/).slice(1)
+        const uaOverride = config.uaOverride
+        const q = args.join(' ')
+        const ar = args.map((v) => v.toLowerCase())
+        const url = args.length !== 0 ? args[0] : ''
+
+        /********** VALIDATOR **********/
+        const isCmd = body.startsWith(prefix)
+        const isBlocked = blockNumber.includes(sender.id)
+        const isOwner = sender.id === ownerNumber
+        const isBanned = _ban.includes(sender.id)
+        const isPremium = premium.checkPremiumUser(sender.id, _premium)
+        const isRegistered = register.checkRegisteredUser(sender.id, _registered)
+        const isGroupAdmins = isGroupMsg ? groupAdmins.includes(sender.id) : false
+        const isBotGroupAdmins = isGroupMsg ? groupAdmins.includes(botNumber) : false
+        const isNsfw = isGroupMsg ? _nsfw.includes(groupId) : false
+        const isWelcomeOn = isGroupMsg ? _welcome.includes(groupId) : false
+        const isDetectorOn = isGroupMsg ? _antilink.includes(groupId) : false
+        const isLevelingOn = isGroupMsg ? _leveling.includes(groupId) : false
+        const isAutoStickerOn = isGroupMsg ? _autosticker.includes(groupId) : false
+        const isAntiNsfw = isGroupMsg ? _antinsfw.includes(groupId) : false
+        const isMute = isGroupMsg ? _mute.includes(chat.id) : false
+        const isAfkOn = isGroupMsg ? afk.checkAfkUser(sender.id, _afk) : false
+        const isQuotedImage = quotedMsg && quotedMsg.type === 'image'
+        const isQuotedVideo = quotedMsg && quotedMsg.type === 'video'
+        const isQuotedSticker = quotedMsg && quotedMsg.type === 'sticker'
+        const isQuotedGif = quotedMsg && quotedMsg.mimetype === 'image/gif'
+        const isQuotedAudio = quotedMsg && quotedMsg.type === 'audio'
+        const isQuotedVoice = quotedMsg && quotedMsg.type === 'ptt'
+        const isImage = type === 'image'
+        const isVideo = type === 'video'
+        const isAudio = type === 'audio'
+        const isVoice = type === 'ptt'
+        const isGif = mimetype === 'image/gif'
+        /********** END OF VALIDATOR **********/
+
+        // Automate
+        premium.expiredCheck(_premium)
+        cron.schedule('0 0 * * *', () => {
+            const reset = []
+            _limit = reset
+            console.log('Hang tight, it\'s time to reset usage limits...')
+            fs.writeFileSync('./database/user/limit.json', JSON.stringify(_limit))
+            console.log('Success!')
+        }, {
+            scheduled: true,
+            timezone: 'Asia/Jakarta'
+        })
+
+            // ROLE (Change to what you want, or add) and you can change the role sort based on XP.
+            const levelRole = level.getLevelingLevel(sender.id, _level)
+            var role = 'Copper V'
+            if (levelRole >= 5){
+                role = 'Copper IV'
+            } 
+            if (levelRole >= 10){
+                role = 'Copper III'
+            } 
+            if (levelRole >= 15){
+                role = 'Copper II'
+            } 
+            if (levelRole >= 20){
+                role = 'Copper I'
+            } 
+            if (levelRole >= 25) {
+                role = 'Silver V'
+            } 
+            if (levelRole >= 30) {
+                role = 'Silver IV'
+            } 
+            if (levelRole >= 35) {
+                role = 'Silver III'
+            } 
+            if (levelRole >= 40) {
+                role = 'Silver II'
+            } 
+            if (levelRole >= 45) {
+                role = 'Silver I'
+            } 
+            if (levelRole >= 50) {
+                role = 'Gold V'
+            } 
+            if (levelRole >= 55) {
+                role = 'Gold IV'
+            } 
+            if (levelRole >= 60) {
+                role = 'Gold III'
+            } 
+            if (levelRole >= 65) {
+                role = 'Gold II'
+            } 
+            if (levelRole >= 70) {
+                role = 'Gold I'
+            } 
+            if (levelRole >= 75) {
+                role = 'Platinum V'
+            } 
+            if (levelRole >= 80) {
+                role = 'Platinum IV'
+            } 
+            if (levelRole >= 85) {
+                role = 'Platinum III'
+            } 
+            if (levelRole >= 90) {
+                role = 'Platinum II'
+            } 
+            if (levelRole >= 95) {
+                role = 'Platinum I'
+            } 
+            if (levelRole >= 100) {
+                role = 'Exterminator'
+            }
+        if (isGroupMsg && isRegistered && !level.isGained(sender.id) && !isBanned && isLevelingOn) {
+            try {
+                level.addCooldown(sender.id)
+                const currentLevel = level.getLevelingLevel(sender.id, _level)
+                const amountXp = Math.floor(Math.random() * (15 - 25 + 1) + 15)
+                const requiredXp = 5 * Math.pow(currentLevel, 2) + 50 * currentLevel + 100
+                level.addLevelingXp(sender.id, amountXp, _level)
+                if (requiredXp <= level.getLevelingXp(sender.id, _level)) {
+                    level.addLevelingLevel(sender.id, 1, _level)
+                    const userLevel = level.getLevelingLevel(sender.id, _level)
+                    const fetchXp = 5 * Math.pow(userLevel, 2) + 50 * userLevel + 100
+                    await bocchi.reply(from, `*── 「 LEVEL UP 」 ──*\n\n➸ *Name*: ${pushname}\n➸ *XP*: ${level.getLevelingXp(sender.id, _level)} / ${fetchXp}\n➸ *Level*: ${currentLevel} -> ${level.getLevelingLevel(sender.id, _level)} 🆙 \n➸ *Role*: *${role}*`, id)
+                }
+            } catch (err) {
+                console.error(err)
+            }
+        }
+
+        // Anti group link detector
+        if (isGroupMsg && !isGroupAdmins && isBotGroupAdmins && isDetectorOn && !isOwner) {
+            if (chats.match(new RegExp(/(https:\/\/chat.whatsapp.com)/gi))) {
+                const valid = await bocchi.inviteInfo(chats)
+                if (valid) {
+                    console.log(color('[KICK]', 'red'), color('Received a group link and it is a valid link!', 'yellow'))
+                    await bocchi.reply(from, ind.linkDetected(), id)
+                    await bocchi.removeParticipant(groupId, sender.id)
+                } else {
+                    console.log(color('[WARN]', 'yellow'), color('Received a group link but it is not a valid link!', 'yellow'))
+                }
+            }
+        }
+
+        // Anti virtext by: @VideFrelan
+        if (isGroupMsg && !isGroupAdmins && isBotGroupAdmins && !isOwner) {
+           if (chats.length > 5000) {
+               await bocchi.sendTextWithMentions(from, `@${sender.id} is detected sending a virtext.\nYou will be kicked!`)
+               await bocchi.removeParticipant(groupId, sender.id)
+            }
+        } 
+               
+        // Sticker keywords by: @hardianto02_
+        if (isGroupMsg && isRegistered) {
+            if (_stick.includes(chats)) {
+                await bocchi.sendImageAsSticker(from, `./temp/sticker/${chats}.webp`, { author: authorWm, pack: packWm })
+            }
+        }
+
+        // Anti fake group link detector by: Baguettou
+        if (isGroupMsg && !isGroupAdmins && isBotGroupAdmins && isDetectorOn && !isOwner) {
+            if (chats.match(new RegExp(/(https:\/\/chat.(?!whatsapp.com))/gi))) {
+                console.log(color('[KICK]', 'red'), color('Received a fake group link!', 'yellow'))
+                await bocchi.reply(from, 'Fake group link detected!', id)
+                await bocchi.removeParticipant(groupId, sender.id)
+            }
+        }
+
+        // Anti NSFW link
+        if (isGroupMsg && !isGroupAdmins && isBotGroupAdmins && isAntiNsfw && !isOwner) {
+            if (isUrl(chats)) {
+                const classify = new URL(isUrl(chats))
+                console.log(color('[FILTER]', 'yellow'), 'Checking link:', classify.hostname)
+                isPorn(classify.hostname, async (err, status) => {
+                    if (err) return console.error(err)
+                    if (status) {
+                        console.log(color('[NSFW]', 'red'), color('The link is classified as NSFW!', 'yellow'))
+                        await bocchi.reply(from, ind.linkNsfw(), id)
+                        await bocchi.removeParticipant(groupId, sender.id)
+                    } else {
+                        console.log(('[NEUTRAL]'), color('The link is safe!'))
+                    }
+                })
+            }
+        }
+
+        // Auto sticker
+        if (isGroupMsg && isAutoStickerOn && isMedia && isImage && !isCmd) {
+            const mediaData = await decryptMedia(message, uaOverride)
+            const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`
+            await bocchi.sendImageAsSticker(from, imageBase64, { author: authorWm, pack: packWm })
+            console.log(`Sticker processed for ${processTime(t, moment())} seconds`)
+        }
+
+        // Auto sticker video
+        if (isGroupMsg && isAutoStickerOn && isMedia && isVideo && !isCmd) {
+            const mediaData = await decryptMedia(message, uaOverride)
+            const videoBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`
+            await bocchi.sendMp4AsSticker(from, videoBase64, { stickerMetadata: true, pack: packWm, author: authorWm, fps: 30, startTime: '00:00:00.0', endTime : '00:00:05.0', crop: false, loop: 0 })
+            console.log(`Sticker processed for ${processTime(t, moment())} seconds`)
+        }
+
+        // AFK by Slavyan
+        if (isGroupMsg) {
+            for (let ment of mentionedJidList) {
+                if (afk.checkAfkUser(ment, _afk)) {
+                    const getId = afk.getAfkId(ment, _afk)
+                    const getReason = afk.getAfkReason(getId, _afk)
+                    const getTime = afk.getAfkTime(getId, _afk)
+                    await bocchi.reply(from, ind.afkMentioned(getReason, getTime), id)
+                }
+            }
+            if (afk.checkAfkUser(sender.id, _afk) && !isCmd) {
+                _afk.splice(afk.getAfkPosition(sender.id, _afk), 1)
+                fs.writeFileSync('./database/user/afk.json', JSON.stringify(_afk))
+                await bocchi.sendText(from, ind.afkDone(pushname))
+            }
+        }
+
+        // Mute
+        if (isCmd && isMute && !isGroupAdmins && !isOwner && !isPremium) return
+        
+        // Ignore banned and blocked users
+        if (isCmd && (isBanned || isBlocked) && !isGroupMsg) return console.log(color('[BAN]', 'red'), color(time, 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname))
+        if (isCmd && (isBanned || isBlocked) && isGroupMsg) return console.log(color('[BAN]', 'red'), color(time, 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname), 'in', color(name || formattedTitle))
+
+        // Anti spam
+        if (isCmd && msgFilter.isFiltered(from) && !isGroupMsg) return console.log(color('[SPAM]', 'red'), color(time, 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname))
+        if (isCmd && msgFilter.isFiltered(from) && isGroupMsg) return console.log(color('[SPAM]', 'red'), color(time, 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname), 'in', color(name || formattedTitle))
+
+        // Log
+        if (isCmd && !isGroupMsg) {
+            console.log(color('[CMD]'), color(time, 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname))
+            await bocchi.sendSeen(from)
+        }
+        if (isCmd && isGroupMsg) {
+            console.log(color('[CMD]'), color(time, 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname), 'in', color(name || formattedTitle))
+            await bocchi.sendSeen(from)
+        }
+        
+        // Anti spam
+        if (isCmd && !isPremium && !isOwner) msgFilter.addFilter(from)
